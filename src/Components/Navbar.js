aeb96f04b7c7fb71894f5d9daa2428eeec1ce745
mutabazichristian <c.mutabazi@alustudent.com>
2023-11-22 11:38:05 +0200

    adding a log out button and mechanism

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,10 +4,19 @@ import ListNavigationList from "./ListNavigation";
 import BtnNewExpense from "./BtnNewExpense";
 import { useRef } from "react";
 
+const handleLogout = () => {
+    const cookies = document.cookie.split(';');
+    cookies.forEach(cookie => {
+        const cookieSections = cookie.split('=');
+        const cookieName = cookieSections[0].trim();
+        document.cookie = `${cookieName}=;expires=Thu,01 Jan 1970 00:00:00 UTC; path=/;`;
+    })
+}
 function Navbar() {
     return (
         <div className="navbar" >
-            <img src={profileImage} alt="profile" />
+            {/* <img src={profileImage} alt="profile" /> */}
+            <button onClick={handleLogout}>Log out</button>
             <ListNavigationList />
             <BtnNewExpense />
         </div>
