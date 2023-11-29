cad63a6a08a314f645ca0514cfb15b938ce24976
mutabazichristian <c.mutabazi@alustudent.com>
2023-11-29 12:38:47 +0200

    create

--- src/Pages/SystemAdminPage.js
+++ src/Pages/SystemAdminPage.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UserAdminsView from '../Components/UserAdminsView';
 import { useNavigate } from 'react-router-dom';
 
 function SystemAdminPage(props) {
+    //new user admin data
+    const [username, setUsername] = useState;
+    const [password, setPassword] = useState;
+    const [repeatPassword, setRepeatPassword] = useState;
+    const [email, setEmail] = useState;
+
     const { setUserType } = props;
     const navigate = useNavigate();
     var userAdmins = [];
@@ -19,6 +25,11 @@ function SystemAdminPage(props) {
         setUserType('');
         navigate('/');
     }
+
+    const handleCreateNewUserAdmin = async (event) => {
+        event.preventDefault();
+        console.log('tryna create a new admin huh?');
+    }
     return (
         <div>
             <h1>System Admin <button onClick={handleLogout}> Log Out</button></h1>
@@ -34,8 +45,28 @@ function SystemAdminPage(props) {
                 }
             </ul>
             <h1>Create New User Admin</h1>
-            <form action="">
-
+            <form onSubmit={handleCreateNewUserAdmin}>
+                <table>
+                    <tr>
+                        <td><label htmlFor="">Username</label></td>
+                        <td><input type="text" name="username" onChange={e => { setUsername(e.target.value) }} /></td>
+                    </tr>
+                    <tr>
+                        <td><label htmlFor="">Email</label></td>
+                        <td><input type="text" name='email' onCanPlay={e => { setEmail(e.target.value) }} /></td>
+                    </tr>
+                    <tr>
+                        <td><label htmlFor="">Password</label></td>
+                        <td><input type="password" name="password" onChange={e => { setPassword(e.target.value) }} /></td>
+                    </tr>
+                    <tr>
+                        <td><label htmlFor="">Repeat Password</label></td>
+                        <td><input type="password" name="repeatPassword" onChange={e => { setRepeatPassword(e.target.value) }} /></td>
+                    </tr>
+                    <tr>
+                        <button type='submit'>Create New User Admin</button>
+                    </tr>
+                </table>
             </form>
             <h1>Create New System Admin</h1>
             <form action="">
